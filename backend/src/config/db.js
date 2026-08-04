c249cedbecc3b31597b2a74955f7eae870155222
alokkksharmaa <aloksharma1097@gmail.com>
2026-08-04 10:09:45 +0530

    commits

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,15 @@
-import mongoose from 'mongoose';
-
-export const connectDB = async () => {
+import { MongoClient } from 'mongodb';
+const client = new MongoClient("mongodb+srv://aloksharma1097_db_user:A4o7qGY9Ow10NINi@commitcanvas.vm7gp2a.mongodb.net/?appName=commitcanvas");
+export async function connectToMongoDB() {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      serverSelectionTimeoutMS: 5000,
-    });
-    console.log(`✅ MongoDB connected: ${conn.connection.host}`);
+    await client.connect();
+    console.log("You successfully connected to MongoDB!");
+    return client;
   } catch (err) {
-    console.error('❌ MongoDB connection error:', err.message);
-    process.exit(1);
+    console.dir(err);
   }
-};
+}
+// Call this only when your application terminates
+export async function disconnectFromMongoDB() {
+  await client.close();
+}
